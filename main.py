e69a6abe8f6bbe8ad8dd58cb07ebed4ca5e1b037
JacobColbyTanner <59493314+JacobColbyTanner@users.noreply.github.com>
2024-09-18 16:39:28 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,210 @@
+
+# Define networks
+
+import sys
+import os
+
+# Add the path to your local neurogym package
+local_neurogym_path = os.path.abspath('/Users/jacobtanner/neurogym_match_to_discrete/neurogym')
+if local_neurogym_path not in sys.path:
+    sys.path.insert(0, local_neurogym_path)
+
+# Now import neurogym
+import neurogym as ngym
+
+import torch
+import torch.nn as nn
+from torch.nn import init
+from torch.nn import functional as F
+import math
+from CTRNN import RNNNet, CTRNN
+import gym
+import torch.optim as optim
+from gym.envs.registration import register
+
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt 
+import numpy as np
+
+register(id = 'FindSensePairs-v0',entry_point= "neurogym.envs.FindSensePairs:FindSensePairs")
+
+
+
+env = gym.make('FindSensePairs-v0')  # Replace 'YourEnvName-v0' with the ID you used during registration
+
+
+
+# Environment
+task = 'FindSensePairs-v0'
+kwargs = {'dt': 100, 'N': 4}
+seq_len = 100
+
+
+# Make supervised dataset
+dataset = ngym.Dataset(task, env_kwargs=kwargs, batch_size=16,
+                       seq_len=seq_len)
+
+# A sample environment from dataset
+env = dataset.env
+# Visualize the environment with 2 sample trials
+_ = ngym.utils.plot_env(env, num_trials=2)
+
+# Network input and output size
+input_size = env.observation_space.shape[0]
+output_size = env.action_space.n
+
+
+
+
+
+
+
+# Instantiate the network and print information
+hidden_size = 64
+net = RNNNet(input_size=input_size, hidden_size=hidden_size,
+             output_size=output_size, dt=env.dt)
+print(net)
+
+# Use Adam optimizer
+optimizer = optim.Adam(net.parameters(), lr=0.01)
+criterion = nn.CrossEntropyLoss()
+
+running_loss = 0
+running_acc = 0
+for i in range(1000):
+    inputs, labels = dataset()
+    inputs = torch.from_numpy(inputs).type(torch.float)
+    labels = torch.from_numpy(labels.flatten()).type(torch.long)
+
+    # in your training loop:
+    optimizer.zero_grad()   # zero the gradient buffers
+    output, _ = net(inputs)
+    output = output.view(-1, output_size)
+    loss = criterion(output, labels)
+    loss.backward()
+    optimizer.step()    # Does the update
+
+    running_loss += loss.item()
+    if i % 100 == 99:
+        running_loss /= 100
+        print('Step {}, Loss {:0.4f}'.format(i+1, running_loss))
+        running_loss = 0
+
+
+    
+
+env.reset(no_step=True)
+perf = 0
+num_trial = 100
+activity_dict = {}
+trial_infos = {}
+all_inputs = []
+for i in range(num_trial):
+    env.new_trial()
+    ob, gt = env.ob, env.gt
+    inputs = torch.from_numpy(ob[:, np.newaxis, :]).type(torch.float)
+    all_inputs.append(inputs.detach().numpy().squeeze())
+    action_pred, rnn_activity = net(inputs)
+    rnn_activity = rnn_activity[:, 0, :].detach().numpy()
+    activity_dict[i] = rnn_activity
+    trial_infos[i] = env.trial
+    
+# Concatenate activity for PCA
+activity = np.concatenate(list(activity_dict[i] for i in range(num_trial)), axis=0)
+print('Shape of the neural activity: (Time points, Neurons): ', activity.shape)
+
+
+# Compute PCA and visualize
+
+
+pca = PCA(n_components=3)
+pca.fit(activity)
+
+plt.figure()
+for i in range(num_trial):
+    activity_pc = pca.transform(activity_dict[i])
+    trial = trial_infos[i]
+    color = 'red' if trial['ground_truth'] == 0 else 'blue'
+    plt.plot(activity_pc[:, 0], activity_pc[:, 1], 'o-', color=color)
+
+plt.show()
+
+
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+for i in range(num_trial):
+    activity_pc = pca.transform(activity_dict[i])
+    trial = trial_infos[i]
+    if trial_infos[i]['pair'][0] == 1:
+        color = 'red'
+    elif trial_infos[i]['pair'][0] == 2:
+        color = 'green'
+    elif trial_infos[i]['pair'][0] == 3:
+        color = 'black'
+    elif trial_infos[i]['pair'][0] == 4:
+        color = 'yellow'
+    elif trial['ground_truth'] == 0:
+        color = 'blue'
+    
+    ax.plot(activity_pc[:, 0], activity_pc[:, 1], activity_pc[:, 2], 'o-', color=color)
+
+ax.set_xlabel('PC1')
+ax.set_ylabel('PC2')
+ax.set_zlabel('PC3')
+
+plt.show()
+
+
+pairs = []
+for i in range(num_trial):
+    pairs.append(trial_infos[i]['pair'])
+
+pairs = np.array(pairs)
+
+print(pairs.shape)
+
+#index where first column of pairs is 1 and second column is 3
+idx = pairs[:,0] == 1
+idx2 = pairs[:,1] == 3
+#find the first instance where both are true
+idx = idx & idx2
+print(idx)
+idx = np.argmax(idx)
+
+#find first nonzero in idd (a Boolean array)
+
+idxx = np.nonzero(idx)
+
+#activity_pc = pca.transform(activity_dict[idxx])
+
+#Make subplots with plt.subplots
+fig, axs = plt.subplots(4)
+axs[0].imshow(all_inputs[:,idxx[0]].T)
+axs[1].imshow(activity_dict[:,idxx[0]].T)
+
+
+
+#index where first column of pairs is 1 and second column is 3
+idx = pairs[:,0] == 2
+idx2 = pairs[:,1] == 4
+#find the first instance where both are true
+idx = idx & idx2
+print(idx)
+idx = np.argmax(idx)
+
+
+#find first nonzero in idd (a Boolean array)
+print(idx)
+idyy = np.nonzero(idx)
+
+#activity_pc = pca.transform(activity_dict[idxx])
+
+
+axs[2].imshow(all_inputs[:,idyy[0]].T)
+axs[3].imshow(activity_dict[:,idyy[0]].T)
+plt.show()
